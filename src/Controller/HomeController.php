51c27485daf0d670662f5b8c0cc157cd7870d26a
Jérémy DUTHEIL <dutheil.jeremy@gmail.com>
2018-01-04 18:37:12 +0100

    first

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function index()
+	{
+
+		return $this->render('home/index.html.twig', array());
+	}
+}
