9e2a289a2d974c384766f4d11fab55704ab571de
Jérémy DUTHEIL <dutheil.jeremy@gmail.com>
2020-03-29 16:04:11 +0200

    yarn / ...

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.scss';
+
+import $ from 'jquery';
+
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+
+$(document).ready(function() {
+    console.log('app.js loaded');
+});
