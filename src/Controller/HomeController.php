9f1121826687953fb2fc2af5e2ae10b5045ed231
Jérémy DUTHEIL <dutheil.jeremy@gmail.com>
2020-03-30 10:50:27 +0200

    http cache

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class HomeController extends Controller
 {
 	/**
@@ -12,9 +14,13 @@ class HomeController extends Controller
 	 */
 	public function index()
 	{
+		$response = $this->render('home/index.html.twig', array());
 
-		return $this->render('home/index.html.twig', array());
+		$response->setSharedMaxAge(3600);
+		$response->headers->addCacheControlDirective('must-revalidate', true);
+
+		return $response;
 	}
 
-	
+
 }
