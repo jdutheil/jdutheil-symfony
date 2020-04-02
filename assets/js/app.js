dac6d74c134dbade2782b071fd5e70c702166689
Jérémy DUTHEIL <dutheil.jeremy@gmail.com>
2020-04-02 11:10:15 +0200

    ga

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,22 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 import 'lazysizes';
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+var internal = readCookie('InternalTraffic');
+if (internal != null) {
+    console.log('internal');
+    ga('set', 'dimension1', 'true');
+} else {
+    console.log('external');
+}
